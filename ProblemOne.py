3691af3ccdca08e8e96bef774d835d40ca788a4e
LuisJC-creator <luis.chavez04@utrgv.edu>
2025-10-06 21:48:38 +0000

    Homework 1 done

--- ProblemOne.py
+++ ProblemOne.py
@@ -1,44 +1,83 @@
-class Computer:
-    def __init__(self, idefNum, isInf, conList):
-        self.idefNum = idefNum 
-        self.isInf = isInf 
-        self.conList = conList
-    
-    def setInfStat(status):
-        self.isInf = status
-    
-    def getInfStat():
-        return self.isInf
-        
+# homework 1 python
 
+class Computer:
+    def __init__(self, ident):
+        self.ident = ident
+        self.isInf = False
+        self.conList = []   # list of connected computer numbers
+
+    def setInfStat(self, status):
+        self.isInf = status
+
+    def getInfStat(self):
+        return self.isInf
+
+    def addConnection(self, other_id):
+        self.conList.append(other_id)
+
+    def getConnections(self):
+        return self.conList
+
+# open the file for reading
 f = open("input.txt", "r")
 lines = f.readlines()
+f.close()
 
-compList = []
-numComputer = lines[0]
-numEdges = lines[1]
-edgelist = []
-tempConctList = []
+num_computers = int(lines[0])
+num_edges = int(lines[1])
 
-#loop for filling out list of connections
-for x in range(2, numEdges+1):
-    tempConctList.append(lines[x])
+# list of computer objects creation loop
+computers = [None]
+i = 1
+while i <= num_computers:
+    computers.append(Computer(i))
+    i = i + 1
 
-# loop for creating computer objects
-for i in range(numComputer):
-    tempC = Computer()
+# edges
+edge_index = 2
+end_edge_index = 2 + num_edges # to end at the right index for edges in input.txt
+while edge_index < end_edge_index:
+    parts = lines[edge_index].split()
+    a = int(parts[0]) # comment this better if you have time.
+    b = int(parts[1])
+    computers[a].addConnection(b)
+    computers[b].addConnection(a)
+    edge_index = edge_index + 1
 
+# starting infected
+start = int(lines[end_edge_index])
+computers[start].setInfStat(True)
 
-for i in range(2, numEdges):
-    edge = lines[i]
-    edgelist.append(lines[i])
-    print(edgelist)
+# checking the lists and updating infection statuses
+infected = [start]   # list of infected 
+idx = 0
+while idx < len(infected):
+    current_id = infected[idx]
+    neighbors = computers[current_id].getConnections()
 
-infComp = input("Please enter whcih computer is infected: ")
+    j = 0
+    while j < len(neighbors):
+        nb = neighbors[j]
 
-# find if computer is "connected" i.e. edge has same
-# for x in range(len(edgelist)): # traverse main list
- #   for y in range(x):
-        
+        # check if nb already infected
+        k = 0
+        seen = False
+        while k < len(infected):
+            if infected[k] == nb:
+                seen = True
+            k = k + 1
 
-   #  for y in range(): # this loop looks at the specific lists within our list of lists
+        if (not seen) and (not computers[nb].getInfStat()):
+            computers[nb].setInfStat(True)
+            infected.append(nb)
+        j = j + 1
+
+    idx = idx + 1
+
+# exclude the original infected computer
+answer = len(infected) - 1
+
+print(answer)
+outf = open("output.txt", "w")
+outf.write(str(answer))
+outf.close()
